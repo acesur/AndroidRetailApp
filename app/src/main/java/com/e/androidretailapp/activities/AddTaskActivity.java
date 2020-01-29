cbf8f50481292310e54b388651ab16f34de52b40
Softwarica College Lab <sures.chaudhary0@gmail.com>
2020-01-29 13:46:31 +0545

    Initial commit

---
+++ app/src/main/java/com/e/androidretailapp/activities/AddTaskActivity.java
@@ -0,0 +1,68 @@
+package com.e.androidretailapp.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.e.androidretailapp.R;
+import com.e.androidretailapp.api.TaskAPI;
+import com.e.androidretailapp.url.Url;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    Button btnAddNotes;
+    EditText etNote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_notes);
+
+        btnAddNotes = findViewById(R.id.btnAddNote);
+        etNote = findViewById(R.id.etNote);
+
+        btnAddNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotes();
+            }
+        });
+    }
+
+    private void addNotes() {
+        //Task notes = new Task(etNote.getText().toString());
+
+        TaskAPI noteAPI = Url.getInstance().create(TaskAPI.class);
+
+        Call<Void> voidCall = noteAPI.addTask(Url.token,etNote.getText().toString());
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(AddTaskActivity.this, "Code : " + response.code() + ", Message : " + response.message(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(AddTaskActivity.this, "Added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(AddTaskActivity.this, "Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+}

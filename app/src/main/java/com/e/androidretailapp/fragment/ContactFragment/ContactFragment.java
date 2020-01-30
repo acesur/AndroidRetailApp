0458c568a7d967cdba678c1b222a12f8af9f6efd
Softwarica College Lab <sures.chaudhary0@gmail.com>
2020-01-30 09:12:59 +0545

    Phone Dial Up Explisit

--- app/src/main/java/com/e/androidretailapp/fragment/ContactFragment/ContactFragment.java
+++ app/src/main/java/com/e/androidretailapp/fragment/ContactFragment/ContactFragment.java
@@ -1,6 +1,8 @@
 package com.e.androidretailapp.fragment.ContactFragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.e.androidretailapp.R;
 
@@ -15,7 +19,10 @@ import com.e.androidretailapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class ContactFragment extends Fragment {
+    private ContactViewModel contactViewModel;
 
+    private EditText etPhoneNo;
+    private Button btnDial;
 
     public ContactFragment() {
         // Required empty public constructor
@@ -26,7 +33,26 @@ public class ContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View root = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        etPhoneNo = root.findViewById(R.id.etPhoneNo);
+        btnDial = root.findViewById(R.id.btnDial);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dial();
+            }
+        });
+
+    return  root;
+    }
+
+    private void Dial() {
+        Uri u = Uri.parse("tel:" + etPhoneNo.getText().toString());
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0123456789"));
+        startActivity(intent);
     }
 
 }

e413d6562dba3b1f7ad2bf865400442959809717
acesur <suresh.chaudhary0@gmail.com>
2020-02-07 14:51:58 +0545

    Product API Generated For Products

--- app/src/main/java/com/e/androidretailapp/api/ProductAPI.java
+++ app/src/main/java/com/e/androidretailapp/api/ProductAPI.java
@@ -1,4 +1,36 @@
 package com.e.androidretailapp.api;
 
+import com.e.androidretailapp.model.Product;
+import com.e.androidretailapp.serverresponse.ImageResponse;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
 public interface ProductAPI {
+
+
+    @POST("products")
+    Call<Product> addProduct(@Body Product product);
+
+    @FormUrlEncoded
+    @POST("users")
+    Call<Product> checkUser(@Field("productName") String ProductName, @Field("productDescription") String ProductDescription, @Field("productManufacture") String ProductManufacture, @Field("productPrice") String ProductPrice, @Field("productQuantity") String ProductQuantity, @Field("productUses") String ProductUses);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
+    @GET("products/me")
+    Call<Product> getProductDetails(@Header("Authorization") String token);
+
+
 }

0cbbdf1f0611008310934b16bb1b7e0c5ad95ec7
Softwarica College Lab <softwarica@softwarica.com>
2020-02-09 14:56:02 +0545

    Product Adapter

--- app/src/main/java/com/e/androidretailapp/fragment/CartFragment/CartFragment.java
+++ app/src/main/java/com/e/androidretailapp/fragment/CartFragment/CartFragment.java
@@ -4,17 +4,29 @@ package com.e.androidretailapp.fragment.CartFragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.e.androidretailapp.R;
+import com.e.androidretailapp.adapter.ProductAdapter;
+import com.e.androidretailapp.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CartFragment extends Fragment {
+
+    public static List<Product> productList = new ArrayList<>();
+
+    RecyclerView recyclerView;
+    RecyclerView rvProduct;
 
 
     public CartFragment() {
@@ -26,7 +38,17 @@ public class CartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View root = inflater.inflate(R.layout.fragment_cart, container, false);
+
+        recyclerView = root.findViewById(R.id.productRecyclerView);
+        rvProduct = root.findViewById(R.id.rvProducts);
+
+        ProductAdapter productAdapter = new ProductAdapter(CartFragment.productList);
+        recyclerView.setAdapter(productAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return root;
     }
 
 }
